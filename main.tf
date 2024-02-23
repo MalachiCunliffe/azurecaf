e0624d3c423874f8e4bced14dd978101f67fae30
MalachiCunliffe <mal.cunliffe@gmail.com>
2024-02-23 14:58:07 +1300

    updated config

--- main.tf
+++ main.tf
@@ -10,8 +10,14 @@ terraform {
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  client_id       = "d8e498c9-1cec-4e12-9d0d-a654b2883c3a"
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = "6832d73c-44d0-4185-8125-4724dbdac761"
 }
 
 # You can use the azurerm_client_config data resource to dynamically
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "client_secret" {
+  default     = null
+  description = "Client secret for the azure service principle"
+}
+
+variable "tenant_id" {
+  default     = null
+  description = "Tenant id for the azure tenant"
+}
