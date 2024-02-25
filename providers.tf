32ac84a20480b6236f3517e7de0a64da60feb61e
MalachiCunliffe <mal.cunliffe@gmail.com>
2024-02-26 11:41:06 +1300

    add subscription_id to providers.tf

--- providers.tf
+++ providers.tf
@@ -24,5 +24,5 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id = "6832d73c-44d0-4185-8125-4724dbdac761"
+  subscription_id = var.subscription_id
 }
