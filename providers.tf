524cf2fcf2876b63e7c5fe8593f8f578cf431481
MalachiCunliffe <mal.cunliffe@gmail.com>
2024-02-27 16:03:08 +1300

    use aliases

--- providers.tf
+++ providers.tf
@@ -4,7 +4,6 @@
 terraform {
   cloud {
     organization = "kiwis"
-
     workspaces {
       name = "azurecaf"
     }
@@ -13,6 +12,10 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.74.0"
+      configuration_aliases = [
+        azurerm.connectivity,
+        azurerm.management,
+      ]
     }
   }
 }
@@ -25,4 +28,28 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+}
+
+# Declare an aliased provider block using your preferred configuration.
+# This will be used for the deployment of all "Connectivity resources" to the specified `subscription_id`.
+
+provider "azurerm" {
+  features {}
+  alias           = "connectivity"
+  subscription_id = "9de08185-b8bb-4d5e-8b60-03940cbcf8f4"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Declare a standard provider block using your preferred configuration.
+# This will be used for the deployment of all "Management resources" to the specified `subscription_id`.
+
+provider "azurerm" {
+  features {}
+  alias           = "management"
+  subscription_id = "9fcd3b10-827a-44e1-8298-628ffc9f1c3e"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
