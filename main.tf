c9c4e6e5c9335552cc1742c3638dec85fce42255
MalachiCunliffe <mal.cunliffe@gmail.com>
2024-03-22 13:41:34 +1300

    update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "enterprise_scale" {
     azurerm.management   = azurerm.management
   }
 
-  root_parent_id = data.azurerm_management_group.caf.id #data.azurerm_client_config.core.tenant_id
+  root_parent_id = "caf" #data.azurerm_management_group.caf.id #data.azurerm_client_config.core.tenant_id
   root_id        = var.root_id
   root_name      = var.root_name
   library_path   = "${path.root}/lib"
